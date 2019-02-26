590e0daf308d6378390f3935adb9663c976a4f6e
Adubbz <Adubbz@users.noreply.github.com>
2019-02-26 12:38:24 +1100

    Start work on skeletoning LocationResolverManager

---
+++ ncm/source/lr_manager_service.hpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 Adubbz
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#include <switch.h>
+#include <stratosphere.hpp>
+#include "lr_location_resolver.hpp"
+#include "lr_registered_location_resolver.hpp"
+#include "lr_add_on_content_location_resolver.hpp"
+
+enum LrManagerCmd
+{
+    Lrm_Cmd_OpenLocationResolver = 0,
+    Lrm_Cmd_OpenRegisteredLocationResolver = 1,
+    Lrm_Cmd_RefreshLocationResolver = 2,
+    Lrm_Cmd_OpenAddOnContentLocationResolver = 3,
+};
+
+class LocationResolverManagerService final : public IServiceObject
+{
+    private:
+        /* Actual commands. */
+        Result OpenLocationResolver(Out<std::shared_ptr<ILocationResolverInterface>> out, FsStorageId storage_id);
+        Result OpenRegisteredLocationResolver(Out<std::shared_ptr<RegisteredLocationResolverInterface>> out);
+        Result RefreshLocationResolver(FsStorageId storage_id);
+        Result OpenAddOnContentLocationResolver(Out<std::shared_ptr<AddOnContentLocationResolverInterface>> out);
+
+    public:
+        DEFINE_SERVICE_DISPATCH_TABLE
+        {
+            MakeServiceCommandMeta<Lrm_Cmd_OpenLocationResolver, &LocationResolverManagerService::OpenLocationResolver>(),
+            MakeServiceCommandMeta<Lrm_Cmd_OpenRegisteredLocationResolver, &LocationResolverManagerService::OpenRegisteredLocationResolver>(),
+            MakeServiceCommandMeta<Lrm_Cmd_RefreshLocationResolver, &LocationResolverManagerService::RefreshLocationResolver>(),
+        
+            /* 2.0.0- */
+            MakeServiceCommandMeta<Lrm_Cmd_OpenAddOnContentLocationResolver, &LocationResolverManagerService::OpenAddOnContentLocationResolver, FirmwareVersion_200>(),
+        };
+};
